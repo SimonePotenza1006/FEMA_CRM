d76c5d95f891ae08ac078d2c31451ef23d5541c4
briziope <60689588+briziope@users.noreply.github.com>
2024-10-18 17:39:54 +0200

    fggfgf

--- lib/pages/DettaglioInterventoPage.dart
+++ lib/pages/DettaglioInterventoPage.dart
@@ -107,7 +107,7 @@ class _DettaglioInterventoPageState extends State<DettaglioInterventoPage> {
 
     if (pickedTime != null) {
       setState(() {
-          print('');
+          print('mostrare orario appena modificato');
         _selectedTimeAppuntamento = pickedTime;
       });
     }
