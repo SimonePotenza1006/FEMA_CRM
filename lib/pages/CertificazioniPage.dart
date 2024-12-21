71d8749aa91a86ffccdb7488f5973a0d63a53bdb
briziope <60689588+briziope@users.noreply.github.com>
2024-12-21 12:12:18 +0100

    gfgfgf

--- lib/pages/CertificazioniPage.dart
+++ lib/pages/CertificazioniPage.dart
@@ -168,7 +168,7 @@ class _DirectoryViewWrapperState extends State<DirectoryViewWrapper> {
   String ipaddress = 'http://gestione.femasistemi.it:8090';
   String ipaddressProva = 'http://gestione.femasistemi.it:8095';
   String ipaddress2 = 'http://192.168.1.248:8090';
-      String ipaddressProva2 = 'http://192.168.1.198:8095';
+  String ipaddressProva2 = 'http://192.168.1.198:8095';
 
   @override
   void initState() {
